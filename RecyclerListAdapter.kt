f66ad35e0e397c62f67f0b61d0a250ddb37f0947
Sumit Ojha <122687829+AndroidDeveloper98@users.noreply.github.com>
2023-03-23 13:04:35 +0530

    Update RecyclerListAdapter.kt

--- RecyclerListAdapter.kt
+++ RecyclerListAdapter.kt
@@ -1,1 +1,39 @@
 
+class RecyclerListAdapter(
+    private var context: Context, private var data: ArrayList<String>
+) : RecyclerView.Adapter<RecyclerListAdapter.SingleViewHolder>() {
+
+    var onClick: OnItemClickListener? = null
+
+    fun setOnItemClickListener(mOnItemClickListener: OnItemClickListener) {
+        this.onClick = mOnItemClickListener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleViewHolder {
+        val binding: ListItemBinding = ListItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return SingleViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+    }
+
+    override fun onBindViewHolder(holder: SingleViewHolder, position: Int) {
+        holder.binding.title.text = "Data"
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    inner class SingleViewHolder(val binding: TimerListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {}
+}
